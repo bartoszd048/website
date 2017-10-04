fcd0ae63a5311d509d31aa7552cd1ba21bb91163
Bartosz Dunajski <bartoszdunajski@gmail.com>
2017-10-04 02:35:27 +0200

    Hello World

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
 #include "cgicc/Cgicc.h"
+#include <cgicc/HTMLClasses.h>
+#include <cgicc/HTTPHTMLHeader.h>
+#include <iostream>
 
-int main(int argc, char **argv) { cgicc::Cgicc formData; }
+int main(int argc, char **argv, char **envp) {
+  std::cout << cgicc::HTTPHTMLHeader() << std::endl;
+
+  std::cout << cgicc::html() << cgicc::head() << cgicc::title("Hello World")
+            << cgicc::head() << std::endl;
+  std::cout << cgicc::body() << std::endl;
+
+  std::cout << cgicc::h1("Title") << std::endl;
+  std::cout << "Body" << std::endl;
+
+  std::cout << cgicc::body() << cgicc::html() << std::endl;
+}
